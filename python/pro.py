cd1ee93dc22d505e2925fdb39802d70af2d740d6
Abdellahsyani <syaniabdos@gmail.com>
2023-06-02 14:22:56 +0200

    calculate the numbre

---
+++ python/pro.py
@@ -0,0 +1,13 @@
+num1 = float(input("Enter first num:"))
+op = input("Enter Operation:"))
+num2 = float(input("Enter second num:"))
+             if op == "+":
+                 print (num1+num2)
+             elif op == "-":
+                 print (num1-num2)
+             elif op == "*":
+                 print (num1*num2)
+             elif op == "/":
+                 print (num1/num2)
+             else:
+                 print ("Please enter valide operation")

7345637ceff3dd827c24d9f680e230836814b4eb
Abdellahsyani <syaniabdos@gmail.com>
2023-06-02 14:05:10 +0200

    new code python

---
+++ python/python.py
@@ -0,0 +1,8 @@
+def max (num1 , num2 num3):
+    if num1 >= num2 and num1 >= num3 :
+        return num1
+    elif num1 >= num2 and num2 >= num3 :
+        return  num2
+    else:
+        return num3
+    print (max(100.10.50))
